a74faa59aee168e78a0b98e99b2ac90a8c2f19a2
GitSomaKey <aeisses@salesforce.com>
2022-06-14 14:45:31 -0300

    Updated the Swift package manager for release 1.0.0.

--- Package.swift
+++ Package.swift
@@ -7,18 +7,18 @@ let package = Package(
     products: [
         .library(
             name: "Swift-InAppMessaging",
-            targets: ["InAppMessagingCore", "InAppMessagingUI"]),
+            targets: ["SMIClientCore", "SMIClientUI"]),
     ],
     targets: [
         .binaryTarget(
-            name: "InAppMessagingCore",
-            url: "",
-            checksum: ""
+            name: "SMIClientCore",
+            url: "https://salesforce-async-messaging.s3.amazonaws.com/public/ios/1.0.0/SMIClientCore-Release.xcframework.zip",
+            checksum: "ea4bf38e63bf0c71487e15e6ede191a4f17929a6c89c01a7bec7a8aa6f2194dd"
         ),
         .binaryTarget(
-            name: "InAppMessagingUI",
-            url: "",
-            checksum: ""
+            name: "SMIClientUI",
+            url: "https://salesforce-async-messaging.s3.amazonaws.com/public/ios/1.0.0/SMIClientUI-Release.xcframework.zip",
+            checksum: "a04ab53ce7083e49731e3e381cc127327b638c87fe6a2f22bac7c39c2c62911b"
         ),
     ]
 )
