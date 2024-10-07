52f23fefddd9d9021ff202ebd4a60ede0f0aa434
Aaron Eisses <aeisses@salesforce.com>
2024-10-07 11:33:15 -0300

    Changing Package.swift file for the 1.7.0 release

--- Package.swift
+++ Package.swift
@@ -12,13 +12,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SMIClientCore",
-            url: "https://salesforce-async-messaging.s3.amazonaws.com/public/ios/1.6.2/SMIClientCore-Release.xcframework.zip",
-            checksum: "d6ddddf30d04d13b169bb960e38e0ee5bb0c089f70d42221d075333e5c6a91e3"
+            url: "https://salesforce-async-messaging.s3.amazonaws.com/public/ios/1.7.0/SMIClientCore-Release.xcframework.zip",
+            checksum: "6faba4213bb921d91f3eff53493ce8a1390a676031fc4eadf66d1c493af15869"
         ),
         .binaryTarget(
             name: "SMIClientUI",
-            url: "https://salesforce-async-messaging.s3.amazonaws.com/public/ios/1.6.2/SMIClientUI-Release.xcframework.zip",
-            checksum: "be5826c8eb89305fbb247b7f6a3e87f19f02bba7896651977ac0cc7a0ea43cac"
+            url: "https://salesforce-async-messaging.s3.amazonaws.com/public/ios/1.7.0/SMIClientUI-Release.xcframework.zip",
+            checksum: "4b1e26e4afbf1660a0f69ad775e2aa51997aa7461a17e40e59522613e5368726"
         ),
     ]
 )
