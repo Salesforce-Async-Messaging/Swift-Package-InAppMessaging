9ab6bf7a2c62486f4ae853a998b9de857d611592
Jeremy Wright <jeremy.wright@salesforce.com>
2026-03-31 11:53:24 -0300

    Changing Package.swift file for the 1.10.4 release

--- Package.swift
+++ Package.swift
@@ -7,18 +7,28 @@ let package = Package(
     products: [
         .library(
             name: "Swift-InAppMessaging",
-            targets: ["SMIClientCore", "SMIClientUI"]),
+            targets: ["SMIClientCore", "SMIClientUI", "SMIClientCoreWrapper"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/sqlcipher/SQLCipher.swift.git", from: "4.10.0")
     ],
     targets: [
         .binaryTarget(
             name: "SMIClientCore",
-            url: "https://salesforce-async-messaging.s3.amazonaws.com/public/ios/1.10.3/SMIClientCore-Release.xcframework.zip",
-            checksum: "3e72dfe4945c9196bf471d40c97a1ed62660e8fd2b5686a6758fefb43be236aa"
+            url: "https://salesforce-async-messaging.s3.amazonaws.com/public/ios/1.10.4/SMIClientCore-Release.xcframework.zip",
+            checksum: "41dfd1ac1fa7e4ec23e06c4c7bb4506aba71ac0b251f54e70e1824ab34d33c67"
         ),
         .binaryTarget(
             name: "SMIClientUI",
-            url: "https://salesforce-async-messaging.s3.amazonaws.com/public/ios/1.10.3/SMIClientUI-Release.xcframework.zip",
-            checksum: "f2de472d324fcdf7e785e5ea0800388e235b5b152b6449c4cd476b09f5cee0ea"
+            url: "https://salesforce-async-messaging.s3.amazonaws.com/public/ios/1.10.4/SMIClientUI-Release.xcframework.zip",
+            checksum: "26b9ba9452864375663aefa300a7f66ce6a112e0d8ecc86083276336bdc15e59"
         ),
+        .target(
+            name: "SMIClientCoreWrapper",
+            dependencies: [
+                "SMIClientCore",
+                .product(name: "SQLCipher", package: "SQLCipher.swift")
+            ]
+        )
     ]
 )
