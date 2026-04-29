3c81d75703b846a94c52dea347692952ffd627bf
Jeremy Wright <jeremy.wright@salesforce.com>
2026-04-29 10:51:28 -0300

    Swap out package to reference the SMIClientCore-iOS.swift SPM as a dependency

--- Sources/SMIClientCoreWrapper/SMIClientCoreWrapper.swift
+++
@@ -1,8 +0,0 @@
-// SMIClientCoreWrapper.swift
-// Re-exports SMIClientCore framework along with its dependencies
-
-// Re-export SMIClientCore and it's binary dependencies
-@_exported import SMIClientCore
-
-// Re-export SQLCipher for encryption capabilities
-@_exported import SQLCipher
---
+++ Sources/SMIClientUIWrapper/SMIClientUIWrapper.swift
@@ -0,0 +1,6 @@
+// SMIClientUIWrapper.swift
+// Re-exports SMIClientCore framework along with its dependencies
+
+// Re-export SMIClientUI and it's binary dependencies
+@_exported import SMIClientUI
+@_exported import SMIClientCore
