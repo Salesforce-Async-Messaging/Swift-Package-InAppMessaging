956f56bb7e02f0e7d91debbcbcfbebdbc480c9a3
Jeremy Wright <jeremy.wright@salesforce.com>
2026-08-12 15:21:48 -0300

    Update Package.swift for 1.11.3 release

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
             targets: ["SMIClientUI", "SMIClientUIWrapper"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Salesforce-Async-Messaging/SMIClientCore-iOS.swift.git", exact: "1.12.0")
+        .package(url: "https://github.com/Salesforce-Async-Messaging/SMIClientCore-iOS.swift.git", exact: "1.11.3")
     ],
     targets: [
         .binaryTarget(
             name: "SMIClientUI",
-            url: "https://salesforce-async-messaging.s3.amazonaws.com/public/ios/1.12.0/SMIClientUI-Release.xcframework.zip",
-            checksum: "79be1c955ffd09dc53607924f22448f552b13dbf424a98a00f05e27e308c777d"
+            url: "https://salesforce-async-messaging.s3.amazonaws.com/public/ios/1.11.3/SMIClientUI-Release.xcframework.zip",
+            checksum: "d171d49131b5b676fa5c8630a76d44883e1edf2cf368542c7e637b431a45b08c"
         ),
         .target(
             name: "SMIClientUIWrapper",
